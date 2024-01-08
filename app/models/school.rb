bf312c1728dc7f923f55454c80034461ca437857
Takahashi-Seiji <takahashiseiji18@gmail.com>
2024-01-08 14:19:21 -0600

    base associations

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,3 @@
 class School < ApplicationRecord
+  has_many :students, through: :school_user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_08_182617) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_08_195604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.bigint "lecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lecture_id"], name: "index_chats_on_lecture_id"
+  end
+
+  create_table "lectures", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.string "content"
+    t.date "date"
+    t.integer "lecture_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lectures_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.bigint "lecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lecture_id"], name: "index_notes_on_lecture_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.bigint "quizz_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quizz_id"], name: "index_questions_on_quizz_id"
+  end
+
+  create_table "quizzs", force: :cascade do |t|
+    t.bigint "lecture_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lecture_id"], name: "index_quizzs_on_lecture_id"
+  end
+
+  create_table "school_users", force: :cascade do |t|
+    t.bigint "school_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["school_id"], name: "index_school_users_on_school_id"
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +92,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_08_182617) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "chats", "lectures"
+  add_foreign_key "lectures", "users"
+  add_foreign_key "messages", "chats"
+  add_foreign_key "notes", "lectures"
+  add_foreign_key "questions", "quizzs"
+  add_foreign_key "quizzs", "lectures"
+  add_foreign_key "school_users", "schools"
 end
