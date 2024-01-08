6630f2bdb0e32ff6c3d564fc2b0862fbdc3bdd36
Takahashi-Seiji <takahashiseiji18@gmail.com>
2024-01-08 15:31:22 -0600

    Added enum status to quizz

--- app/models/quizz.rb
+++ app/models/quizz.rb
@@ -2,4 +2,6 @@ class Quizz < ApplicationRecord
   belongs_to :lecture
   belongs_to :student, class_name: 'User', foreign_key: 'user_id'
   has_many :questions, dependent: :destroy
+
+  enum status: [pending: 1, in_progress: 2, submit: 3]
 end
