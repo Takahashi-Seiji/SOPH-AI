3778da15ebf299b31c5196bcb03ca326db2e91d1
Takahashi-Seiji <takahashiseiji18@gmail.com>
2024-01-08 18:14:52 -0600

    Added basic crud actions to lecture

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -4,5 +4,5 @@ class Lecture < ApplicationRecord
   has_many :notes, dependent: :destroy
   has_many :quizzes, dependent: :destroy
   has_many :student_lectures, dependent: :destroy
-  has_many :students, through: :student_lectures
+  has_many :students, through: :student_lectures, source: :user
 end
