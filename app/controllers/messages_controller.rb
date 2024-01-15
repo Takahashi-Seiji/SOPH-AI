dac797dd5e9b9e0efb250319855ad2ccdab38d7d
Takahashi-Seiji <takahashiseiji18@gmail.com>
2024-01-15 17:33:41 -0600

    Finished chat with AI, finished file upload feature to the creation of the summary. Improved lecture and chat prompts. Added some frontend.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,17 @@
+require "pry-byebug"
 class MessagesController < ApplicationController
   def create
-    @lecture = Lecture.find(params[:lecture_id])
-    @chat = @lecture.chat
+    @chat = Chat.find(params[:chat_id])
+    @lecture = @chat.lecture
     @message = Message.new(message_params)
     @message.chat = @chat
     @message.role = "user"
     @message.user = current_user
     authorize @message
     if @message.save
-      create_ai_message
-      render json: { message: @message.content, role: @message.role, user: @message.user }
+      ai_content = create_ai_message
+      @chat.messages.create!(content: ai_content, role: "assistant", user: current_user)
+      render json: { ai_message: ai_content }
     else
       render "lectures/show"
     end
@@ -18,11 +20,10 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :chat_id)
   end
 
   def create_ai_message
-    content = ChatAiService.new(@message, @lecture).call
-    @chat.messages.create!(content: content, role: "assistant", user: current_user)
+    ChatAiService.new(@message, @lecture).call
   end
 end
